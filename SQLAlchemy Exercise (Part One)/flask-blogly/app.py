a6e3ea6e70032978904a503271edd19af20aea40
spathiyal <spathiyal@gmail.com>
2023-06-29 17:17:11 -0700

    SQLAlchemy

---
+++ SQLAlchemy Exercise (Part One)/flask-blogly/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+
+connect_db(app)
+# db.create_all()
+
+
+@app.route('/')
+def root():
+
+    return redirect("/users")
+
+
+@app.route('/users')
+def home():
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/home.html', users=users)
+
+
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+
+    return render_template('users/new-user.html')
+
+
+@app.route("/users/new", methods=["POST"])
+def new_users():
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show-user.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Handle form submission for updating an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
