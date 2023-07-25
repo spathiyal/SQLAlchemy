25498d7b70c77f0dadaaf4c51fe32b51cc1032ee
spathiyal <spathiyal@gmail.com>
2023-07-25 14:33:22 -0700

    Part 2 exercise

---
+++ part-2/models.py
@@ -0,0 +1,46 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+    with app.app_context():
+        # db.drop_all()
+        db.create_all()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.Text, nullable =False)
+    last_name = db.Column(db.Text, nullable = False)
+    image_url = db.Column(db.Text,nullable = False, default=IMAGE_URL)
+
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.Text, nullable =False)
+    content = db.Column(db.Text, nullable =False)
+    created_at = db.Column(db.DateTime,nullable = False, default=datetime.datetime.now)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable = False)
+
+    @property
+    def friendly_date(self):
+        return self.crated_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+
+
